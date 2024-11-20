6ce556bf4aef5c1bfba3faee530cbfb11804c153
congvt <cong.vt@zinza.com.vn>
2024-11-20 23:41:51 +0700

    fix docker and config env

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    console.log(this.configService.get('MONGO_URI'));
+    return this.configService.get('MONGO_URI');
   }
 }
